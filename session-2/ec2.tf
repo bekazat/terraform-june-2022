c33296c5ae2293d5ccb0a3de82837e64ac790fbc
ec2-user <bekazat@gmail.com>
2022-07-07 02:16:05 +0000

    terraform init, apply, destroy

--- session-2/ec2.tf
+++ session-2/ec2.tf
@@ -1,10 +1,18 @@
-#Every single Terraform Configuration file has a format called .Terraform
+#Every single Terraform Configuration file has a format called .tf
+
+resource "aws_instance" "first_ec2" {
+    ami = "ami-0cff7528ff583bf9a"
+    instance_type = "t2.micro"
+}
 
 
 
+# Syntax is HashiCorp Configuration Language ( HCL )
 
+# Terraform only has 2 blocks 
+# 1. Resource      =  Create and Manage resources
+# 2. Data Source 
 
+# Each block expects label, Resource block expects 2 labels
 
-#Syntax is HashiCorp Configuration Language ( HCL )
-
-
+# resource = block
