d345e2f484140c26906292560a284d17e6f4f7fa
ec2-user <bekazat@gmail.com>
2022-08-02 05:16:56 +0000

    fixed

--- project/main.tf
+++ project/main.tf
@@ -3,11 +3,21 @@ resource "aws_route53_zone" "moneroinc" {
 
 }
 
-resource "aws_route53_record" "nameserver" {
-  name    = "moneroinc.net"
-  ttl     = 3600
-  type    = "NS"
+resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.moneroinc.zone_id
+  name    = "www.moneroinc.net"
+  type    = "A"
+  ttl     = 300
+  records = ["107.23.179.231"]
+}
+
+
+resource "aws_route53_record" "nameserver" {
+  allow_overwrite = true
+  name            = "moneroinc.net"
+  ttl             = 3600
+  type            = "NS"
+  zone_id         = aws_route53_zone.moneroinc.zone_id
 
   records = aws_route53_zone.moneroinc.name_servers
 
