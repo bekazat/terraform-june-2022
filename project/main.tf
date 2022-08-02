6b12dfa7654b40cc2980c1825b64100dee6de222
ec2-user <bekazat@gmail.com>
2022-08-02 04:33:26 +0000

    fixed

--- project/main.tf
+++ project/main.tf
@@ -4,11 +4,11 @@ resource "aws_route53_zone" "moneroinc" {
 }
 
 resource "aws_route53_record" "nameserver" {
-  name            = "moneroinc.net"
-  ttl             = 3600
-  type            = "NS"
-  zone_id         =  aws_route53_zone.moneroinc.zone_id
+  name    = "moneroinc.net"
+  ttl     = 3600
+  type    = "NS"
+  zone_id = aws_route53_zone.moneroinc.zone_id
 
-  records = aws_route53_zone.moneroinc.name_server
+  records = aws_route53_zone.moneroinc.name_servers
 
 }
