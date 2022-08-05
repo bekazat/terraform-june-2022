dbcf13dfd13e13ea7ab2457ab921cb0a2ee16456
ec2-user <bekazat@gmail.com>
2022-08-05 00:48:16 +0000

    updated vpc.tf

--- project/exercise4/vpc.tf
+++ project/exercise4/vpc.tf
@@ -13,8 +13,59 @@ resource "aws_subnet" "udemy-pub-1" {
     vpc_id = aws_vpc.udemy.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "ture"
-    availability_zone = "us-east-1"
+    availability_zone = var.ZONE1
     tags = {
         Name = "udemy-pub-1"
+    }
+}
+
+resource "aws_subnet" "udemy-pub-2" {
+    vpc_id = aws_vpc.udemy.id
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = "ture"
+    availability_zone = var.ZONE2
+    tags = {
+        Name = "udemy-pub-2"
+    }
+}
+
+resource "aws_subnet" "udemy-pub-3" {
+    vpc_id = aws_vpc.udemy.id
+    cidr_block = "10.0.3.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = var.ZONE3
+    tags = {
+        Name = "udemy-pub-3"
+    }
+}
+
+
+resource "aws_subnet" "udemy-priv-1" {
+    vpc_id = aws_vpc.udemy.id
+    cidr_block = "10.0.4.0/24"
+    map_public_ip_on_launch = "ture"
+    availability_zone = var.ZONE1
+    tags = {
+        Name = "udemy-priv-1"
+    }
+}
+
+resource "aws_subnet" "udemy-priv-2" {
+    vpc_id = aws_vpc.udemy.id
+    cidr_block = "10.0.5.0/24"
+    map_public_ip_on_launch = "ture"
+    availability_zone = var.ZONE2
+    tags = {
+        Name = "udemy-priv-2"
+    }
+}
+
+resource "aws_subnet" "udemy-priv-3" {
+    vpc_id = aws_vpc.udemy.id
+    cidr_block = "10.0.6.0/24"
+    map_public_ip_on_launch = "ture"
+    availability_zone = var.ZONE3
+    tags = {
+        Name = "udemy-priv-3"
     }
 }
